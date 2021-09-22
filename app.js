cc57dd00e2233ab1fdb804a763fa4cfdaebb8bce
Arjuna Rawat <arjunarawat@gmail.com>
2021-09-22 20:45:29 +0530

    fix issues

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.use("/api", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next(createError(404));
 });
 
