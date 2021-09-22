60483b532ecc4c64c588897f64a8022f3472bbf2
Arjuna Rawat <arjunarawat@gmail.com>
2021-09-22 18:50:10 +0530

    setup done

---
+++ Constants.js
@@ -0,0 +1,24 @@
+const categories = [
+  {
+    label: "Beauty",
+    ctr: 1,
+    id: 0,
+  },
+  {
+    label: "Educational",
+    ctr: 2.3,
+    id: 1,
+  },
+  {
+    label: "Services",
+    ctr: 1.4,
+    id: 2,
+  },
+  {
+    label: "Gambling",
+    ctr: 0.3,
+    id: 3,
+  },
+];
+
+module.exports = { categories };
---
+++ routes/users.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var router = express.Router();
+const constants = require("../Constants");
+const sendMail = require("../controllers/sendemail");
+router.get("/", function (req, res, next) {
+  res.send({ data: "Api Working" });
+});
+
+router.post("/send_email", function (req, res, next) {
+  console.log("req", req.body);
+  if (req.body?.email) {
+    try {
+      sendMail.main(req.body?.email);
+      res.send({ send: "data", run: "data" });
+    } catch (e) {
+      res.status(404).send({ message: e.message });
+    }
+  } else {
+    res.status(400).send({ message: "Email Not Found" });
+  }
+});
+
+router.get("/categories", function (req, res, next) {
+  res.send({ categories: constants.categories });
+});
+
+module.exports = router;
